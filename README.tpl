0ce17673a28d7b3a0a710742a256130a0401ced5
Beanow <497556+Beanow@users.noreply.github.com>
2023-10-20 19:57:31 +0200

    doc: Generate readme from crate docs

---
+++ README.tpl
@@ -0,0 +1,8 @@
+# KC11B04 Driver &emsp; [![Latest Version]][crates.io] [![Docs Badge]][docs]
+
+[Latest Version]: https://img.shields.io/crates/v/{{crate}}.svg
+[crates.io]: https://crates.io/crates/{{crate}}
+[Docs Badge]: https://docs.rs/{{crate}}/badge.svg
+[docs]: https://docs.rs/{{crate}}
+
+{{readme}}
--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,16 @@
 //!
 //! A 3-pin module using a voltage divider circuit and analog pin to sense a key press.
 //!
-//! ## Wiring notes
+//! # Wiring notes
 //!
 //! The voltage on the `VCC` pin should correspond with the reference voltage (maximum value) your ADC is configured to read.
 //!
 //! For example the default on an Arduino Uno is that it matches the operating voltage of 5V.
 //! But the ADC can be configured to use an external reference with the `AREF` pin. In this case `VCC` should match `AREF`.
+//!
+//! # MSRV & Stability
+//!
+//! This crate compiles on stable Rust `1.60` and up. It _might_ still work on older rust versions, but this isn't ensured. Upgrading the MSRV is considered SemVer breaking.
 
 mod driver;
 mod map;
