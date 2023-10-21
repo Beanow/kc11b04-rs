f8750b7dd28bf7260e70f6ed1a2107f7eccd4b64
Beanow <497556+Beanow@users.noreply.github.com>
2023-10-21 19:37:41 +0200

    doc: Add main example

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,36 @@
 //! For example the default on an Arduino Uno is that it matches the operating voltage of 5V.
 //! But the ADC can be configured to use an external reference with the `AREF` pin. In this case `VCC` should match `AREF`.
 //!
+//! # Example
+//!
+//! ```rust
+//! use embedded_hal_mock::adc::*;
+//! use kc11b04::{Key, KC11B04, MAP_10BIT};
+//!
+//! // Configure your ADC and PIN using the HAL for your ADC.
+//! let mut adc = Mock::new(&[Transaction::read(0, 1023)]);
+//! let pin = MockChan0;
+//!
+//! // Create the keypad driver. Taking ownership of the pin,
+//! // providing a map that matches the resolution of your ADC.
+//! let mut keypad = KC11B04::new(pin, MAP_10BIT);
+//!
+//! // -- somewhere within `loop` --
+//! {
+//! 	// Read current key state.
+//! 	match keypad
+//! 		.key_state(&mut adc)
+//! 		.expect("Problem reading ADC channel")
+//! 	{
+//! 		Some(Key::K4) => { /* K4 key being pressed */ }
+//! 		Some(Key::K3) => { /* K3 key being pressed */ }
+//! 		Some(Key::K2) => { /* K2 key being pressed */ }
+//! 		Some(Key::K1) => { /* K1 key being pressed */ }
+//! 		None => { /* Either nothing, or multiple keys pressed */ }
+//! 	}
+//! }
+//! ```
+//!
 //! # MSRV & Stability
 //!
 //! This crate compiles on stable Rust `1.60` and up. It _might_ still work on older rust versions, but this isn't ensured. Upgrading the MSRV is considered SemVer breaking.
